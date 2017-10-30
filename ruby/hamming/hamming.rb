26b6979e6cd84a6047be46f9a3ab1484da8f91cb
Gurpreet Singh <gurpreet@mailfence.com>
2017-10-29 19:32:08 -0500

    Learned about a cleaner solution

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,22 +1,13 @@
 class Hamming
     def self.compute(compareTo, compareWith)
 
-        compareToChars = compareTo.split('')
-        compareWithChars = compareWith.split('')
-
-        if compareToChars.length != compareWithChars.length
-            raise ArgumentError
+        if compareTo.length != compareWith.length
+            raise ArgumentError, 'strand lengths not equal'
         end
 
-        hamming = 0
-
-        for index in 0..compareToChars.length
-            if compareToChars[index] != compareWithChars[index]
-                hamming += 1
-            end
+        compareTo.chars.zip(compareWith.chars).count do |charTo, charWith|
+            charTo != charWith
         end
-
-        return hamming
     end
 end
 
